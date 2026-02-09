47d3115946e4085523264b94799fba363b5b4e19
VIPULVN <vipulvnandedkar@gmail.com>
2026-02-09 11:49:11 +0530

    Updated the file

--- SQL_Select_execution.sql
+++
@@ -1,6 +0,0 @@
--- Solving Assignment for Day 3, Day 4, Day 5
---1- write a sql to get all the orders where customers name has "a" as second character and "d" as fourth character (58 rows)
-Select * FROM Orders
-Where Customer_Name LIKE '_a_d%'
-
---2- write a sql to get all the orders placed in the month of dec 2020 (352 rows) 
